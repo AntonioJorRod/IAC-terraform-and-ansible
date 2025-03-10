78790cd6dbf1d68103afc87e3b63651038c61786
AntonioJordan <tonijordanrodriguez6@gmail.com>
2025-03-10 20:10:55 +0100

    feat(terraform): initial terraform template

---
+++ environments/dev/main.tf
@@ -0,0 +1,19 @@
+# Aquí van los resource + Config base
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "main_instance"{
+  ami           = "ami-12345678"
+  instance_type = var.instance_type
+}
